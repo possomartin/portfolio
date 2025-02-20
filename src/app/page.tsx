86f30dccd41121b45cb2d7ac9426ba35225f5218
Papi Chulo <martinposso33@gmail.com>
2025-02-19 19:05:33 -0500

    fix(Animated Button): Removed import on Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { AnimatedButton } from './components/animatedButton';
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-open-sans)]">
-      <AnimatedButton title="SEE MY WORK" link="/" />
-    </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-open-sans)]"></div>
   );
 }
